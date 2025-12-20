07bf93393714dabaedaf41564e78776de5cf5d58
Aakrisht Srivastava <aakrisri55@gmail.com>
2025-12-20 12:11:49 +0530

    Add auth functions

--- backend/src/db/models/user.ts
+++ backend/src/db/models/user.ts
@@ -1,4 +1,7 @@
 import mongoose from "mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import { v4 as uuidv4 } from "uuid"
 
 const user = new mongoose.Schema(
   {
@@ -56,5 +59,50 @@ const user = new mongoose.Schema(
     versionKey: false,
   }
 )
+
+user.pre("save", async function () {
+  if (!this.isModified("password")) return
+  
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+user.methods.generateAuthToken = function () {
+  const JWT_SECRET: string | undefined = process.env.JWT_SECRET
+  if (!JWT_SECRET) {
+    console.log("Unable to access JWT_PASSWORD from env")
+    process.exit(1)
+  }
+
+  return jwt.sign(
+    { id: this._id, email: this.email, emailVerified: this.emailVerified },
+    JWT_SECRET,
+    { expiresIn: "1d" }
+  )
+}
+
+user.methods.comparePassword = async function (password: string) {
+  return await bcrypt.compare(password, this.password)
+}
+
+user.methods.generateVerificationToken = function () {
+  const jti = uuidv4()
+  const JWT_SECRET: string | undefined = process.env.JWT_SECRET
+  if (!JWT_SECRET) {
+    console.log("Unable to access JWT_PASSWORD from env")
+    process.exit(1)
+  }
+  const verificationToken = jwt.sign(
+    { id: this._id, email: this.email, iss: "link-fixer", jti },
+    JWT_SECRET,
+    { expiresIn: "1d" }
+  )
+
+  this.verificationToken = verificationToken
+  this.verificationTokenExpires = Date.now() + 24 * 60 * 60 * 1000 // 24 hours
+
+  return verificationToken
+}
+
 const User = mongoose.model("User", user)
 export default User
